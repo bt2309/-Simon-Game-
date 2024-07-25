f91300bbbc592b9609cc20790c60eef6203a00cd
bt23 <brandonyxteo@gmail.com>
2024-07-26 04:03:34 +0800

    w03d05-brandon-0403am

--- js/simongame.js
+++ js/simongame.js
@@ -7,14 +7,6 @@ const BLUE = "Blue";
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-// const game = {
-//     compChoice: [],
-//     playerChoice: [],
-//     turn: "",
-//     status: "",
-//     score: 0,
-// };
-
 let sequence = [];
 let playerSequence = [];
 let level = 0;
@@ -26,46 +18,11 @@ const startButton = document.getElementById("start-button");
 const info = document.querySelector(".info");
 const heading = document.getElementById("game-name");
 const simonBoard = document.querySelector(".simon-board");
-// const resetButton = document.getElementById("reset-button");
-
-// const greenButton = document.getElementById("top-left");
-// const redButton = document.getElementById("top-right");
-// const yellowButton = document.getElementById("btm-left");
-// const blueButton = document.getElementById("btm-right");
-
-// const buttons = [greenButton, redButton, yellowButton, blueButton];
-
-// const scoreCount = document.getElementById("score");
 
 /*-------------------------------  Render Functions --------------------------*/
 
-// const updateMessage = () => {
-
-// };
-
-// const render = () => {
-//     updateMessage();
-//     addToCompChoice();
-//     handlePlayerClick();
-//     yourScore();
-
-// }
 
 /*-------------------------------- Functions --------------------------------*/
-
-// restart game function
-// const init = () => {
-//     game.playerChoice.length = 0;
-//     game.compChoice.length = 0;
-//     for (let i = 0; i < game.compChoice.length; i++) {
-//         game.playerChoice.push("");
-//         game.compChoice.push("");
-//     }
-//     game.turn = game.compChoice;
-//     game.status = "";
-//     render();
-// };
-
 
 //* to commence the beginning of a round of Simon
 const startGame = () => {
@@ -76,15 +33,20 @@ const startGame = () => {
 }
 
 //* to start the next sequence of color clicks
-const nextRound = () => {
+const nextRound = () => { 
     level += 1;
+
+    simonBoard.classList.add("unclickable"); // add back "pointer-event: none" in css
+    info.textContent = "Wait for the computer 😊";
+    heading.textContent = `Level ${level} of 20`; // indicate level reached
+
     const nextSequence = [...sequence]; // copy all values of previous sequence over to the next
     nextSequence.push(nextStep()); // push the comp chosen color into the next sequence array
     playRound(nextSequence); // iterate over the updated sequence with a new color added
 
     sequence = [...nextSequence]; // since player's turn has to take place right after computer's
     setTimeout (() => {           // turn, an artificial delay needs to be added in.
-        humanTurn(level);
+        playerTurn(level);
     }, level * 600 + 1000); // trial and error
 }
 
@@ -123,132 +85,14 @@ const playRound = (nextSequence) => {
 
 //* indicate player's turn to repeat sequence
 const playerTurn = () => {
-    simonBoard.classList.remove("unclickable");
+    simonBoard.classList.remove("unclickable"); // to lift "point-event: none" in css
     info.textContent = "Your Turn 👋";
 }
 
 
-
-
-
-
-
-
-// const startGame = () => {
-//     game.compChoice.push(GREEN, RED); //TODO temporary just to test if my function work
-//     console.log(game.compChoice); //TODO delete aft checking
-// };
-
-// add the next colour to the sequence for Computer
-//TODO ignore till i get the basic sequence to run first
-// const addToCompChoice = () => {
-//     const colors = [GREEN, RED, YELLOW, BLUE];
-//     const randomColor = colors[Math.floor(Math.random() * colors.length)];
-//     game.compChoice.push(randomColor);
-// };
-
-// to check if Player's Choice = Computer's Choice
-
-// const checkChoice = () => {
-//     let playerChoice = game.playerChoice;
-//     let compChoice = game.compChoice;
-
-//     if (playerChoice === compChoice) {
-//         message.textContent = "Keep going!";
-//         return console.log("Won");
-//     }
-//     if (playerChoice !== compChoice) {
-//         message.textContent = "Try Again!"
-//         return console.log("Lost");
-//     }
-// }
-
-// const checkChoice = () => {
-//     if (game.playerChoice.length === game.compChoice.length) {
-//         for (let i = 0; i < game.playerChoice.length; i++) {
-//         if (game.playerChoice[i] !== game.compChoice[i]) {
-//             return console.log("You Lost");  //TODO change aft testing
-//         } else {
-//             return console.log("Won");  //TODO change aft testing
-//         }
-//         }   
-//     } else {
-//         console.log("You Lost");  //TODO change aft testing
-//     }
-// };
-
-// const switchTurn = () => {
-//     if (game.status) {
-//         return;
-//     }
-//     if (game.turn === game.compChoice) {
-//         addToCompChoice();
-//     } else {
-//         game.turn = game.playerChoice;
-//         handlePlayerClick();
-//     }
-// }
-    
-// to add the Player's seleceted color into Player's sequence
-// const handlePlayerClick = (event) => {
-//     if (event.target.classList.contains("simon-btn") &&
-//         game.status === "") {
-//             let pressedColor = event.target;
-//             if (pressedColor === greenButton) {
-//                 game.playerChoice.push(GREEN);
-//             } else if (pressedColor === redButton) {
-//                 game.playerChoice.push(RED);
-//             } else if (pressedColor === yellowButton) {
-//                 game.playerChoice.push(YELLOW);
-//             } else if (pressedColor === blueButton) {
-//                 game.playerChoice.push(BLUE);
-//             }
-//             console.log(game.playerChoice);
-//             // console.log(game);
-//             // checkChoice();
-//             // setTimeout(addToCompChoice, 2000);
-//         }
-// }
-
-// const handlePlayerClick = (event) => {
-//     if (event.target === greenButton) {
-//         game.playerChoice.push(GREEN);
-//     } else if (event.target === redButton) {
-//         game.playerChoice.push(RED);
-//     } else if (event.target === yellowButton) {
-//         game.playerChoice.push(YELLOW);
-//     } else if (event.target === blueButton) {
-//         game.playerChoice.push(BLUE);
-//     }
-//     console.log(game.playerChoice); //TODO delete aft checking
-//     checkChoice();
-// };
-
-
-// checkChoice();
-
-
-// render();
-
-
-// let count = 0;
-// const handlePlayerClick = (event) => {
-//     for (let i = 0; i < buttons.length; i++) {
-//         if (event.target === buttons[i]) {
-//             count++;
-//         }
-//     }
-//     scoreCount.textContent = "Your Score: " + count;
-// };
-
 /*----------------------------- Event Listeners -----------------------------*/
 
 startButton.addEventListener("click", startGame);
-// resetButton.addEventListener("click", init);
 
-// greenButton.addEventListener("click", handlePlayerClick);
-// redButton.addEventListener("click", handlePlayerClick);
-// yellowButton.addEventListener("click", handlePlayerClick);
-// blueButton.addEventListener("click", handlePlayerClick);
 
 
